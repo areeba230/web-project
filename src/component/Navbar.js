8abf44c1e63efc3f71f09aed10d2505b305eb2ea
Areeba <areeba.bscsf19@iba-suk.edu.pk>
2022-08-10 18:48:57 -0700

    added some react logic to open and closed the ham-burger in small screen

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,12 +1,21 @@
-import React from 'react'
-import {link} from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import {link, useLocation} from 'react-router-dom'
 import '../Styles/Navbar.css'
 import ReorderIcon from "@material-ui/icons/Reorder"
 function Navbar() {
+    
+    const [expandNavbar,setExpandNavbar]=useState(false);
+    
+    
+    const location = useLocation();
+    useEffect(() =>{
+        setExpandNavbar(false)
+    },[location])
+
   return (
-    <div className='navbar'>
+    <div className='navbar' id={expandNavbar ? "open" : "closed"}>
         <div className='toggleButton'>
-            <button> <ReorderIcon/></button>
+            <button onClick={()=>{setExpandNavbar((prev) => !prev)}}> <ReorderIcon/></button>
         </div>
         <div className='links'>
             <link to="/">Home</link>
